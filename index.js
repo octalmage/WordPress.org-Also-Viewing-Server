a14e39db696890f7e03899ac7ac11c7cf3155fc5
Jason Stallings <jason@stallin.gs>
2015-01-15 23:52:06 -0600

    Basic page code in place.

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
 var io = require('socket.io')(process.env.PORT || 5000);
-
+var pages=[];
+var users=[];
 
 io.on('connect', function(socket)
 {
 	socket.on('pageopened', function(details)
  	{
     	console.log('username: ' + details.username);
-  		io.sockets.emit('userconnected', { my: 'data' });
+    	console.log('page: ' + details.page);
+    	console.log(socket.id);
+
+    	if (typeof pages[details.page] == "undefined")
+    	{
+    		pages[details.page]=[];
+    	}
+
+    	users[socket.id]={};
+    	users[socket.id].username=details.username;
+    	users[socket.id].page=details.page;
+
+    	if (pages[details.page].indexOf(details.username)==-1)
+    	{
+    		pages[details.page].push(details.username);
+    		console.log(pages);
+    		io.sockets.emit('userconnected', pages);
+    	}
+    	
+  		
   	});
 
 	socket.on('disconnect', function () 
     {
-    	io.sockets.emit('user disconnected');
+    	console.log(socket.id);
+    	io.sockets.emit(users[socket.id].username);
   	});
 });
