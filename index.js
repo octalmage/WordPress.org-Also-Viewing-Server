e1957b429af609cad0cd31147d489633b7271bc3
Jason Stallings <jason@stallin.gs>
2015-01-16 00:54:43 -0600

    It's working\!

--- index.js
+++ index.js
@@ -1,37 +1,43 @@
 var io = require('socket.io')(process.env.PORT || 5000);
+
 var pages=[];
-var users=[];
 
 io.on('connect', function(socket)
 {
 	socket.on('pageopened', function(details)
  	{
     	console.log('username: ' + details.username);
-    	console.log('page: ' + details.page);
-    	console.log(socket.id);
+    	//console.log('page: ' + details.page);
+    	//console.log(socket.id);
 
-    	if (typeof pages[details.page] == "undefined")
-    	{
-    		pages[details.page]=[];
-    	}
-
-    	users[socket.id]={};
-    	users[socket.id].username=details.username;
-    	users[socket.id].page=details.page;
-
-    	if (pages[details.page].indexOf(details.username)==-1)
-    	{
-    		pages[details.page].push(details.username);
-    		console.log(pages);
-    		io.sockets.emit('userconnected', pages);
-    	}
+ 		details.socket=socket.id;
+ 		if (typeof pages[details.page] == "undefined")
+ 		{
+ 			pages[details.page]=[];
+ 		}
+    	pages[details.page].push({username: details.username, page: details.page, socket: socket.id });
+    	io.sockets.emit(details.page, pages[details.page]);
     	
   		
   	});
 
 	socket.on('disconnect', function () 
     {
-    	console.log(socket.id);
-    	io.sockets.emit(users[socket.id].username);
+    	console.log("disconnected.");
+    	console.log(pages);
+    	for (x in pages)
+    	{
+    		for (y in pages[x])
+    		{
+    			if (pages[x][y].socket==socket.id)
+    			{
+    				console.log("disconnect: " + pages[x][y].username);
+    				pages[x].splice(y, 1);
+    				io.sockets.emit(x, pages[x]);
+
+    			}
+    		}
+
+    	}
   	});
 });
