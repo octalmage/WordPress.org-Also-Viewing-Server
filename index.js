e35e40a9bc22bdaab689d59321eca3d75e9275c9
Jason Stallings <jason@stallin.gs>
2015-01-15 22:49:09 -0600

    Added connection code.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 var io = require('socket.io')(process.env.PORT || 5000);
 
+
+io.on('connection', function(socket)
+{
+  socket.on('pageopened', function(details)
+  {
+    console.log('username: ' + details.username);
+  });
+});
