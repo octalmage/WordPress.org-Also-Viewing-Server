21a29bcd111ae75faa9134d1bab5a64290ad484d
Jason Stallings <jason@stallin.gs>
2015-01-15 23:05:00 -0600

    Added user code.

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 var io = require('socket.io')(process.env.PORT || 5000);
 
 
-io.on('connection', function(socket)
+io.on('connect', function(socket)
 {
-  socket.on('pageopened', function(details)
-  {
-    console.log('username: ' + details.username);
-  });
+	socket.on('pageopened', function(details)
+ 	{
+    	console.log('username: ' + details.username);
+  		io.sockets.emit('userconnected', { my: 'data' });
+  	});
+
+	socket.on('disconnect', function () 
+    {
+    	io.sockets.emit('user disconnected');
+  	});
 });
